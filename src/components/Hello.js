51a7bc18fe2f767b310eead647eeb39672e1fb4e
Nandhiniks20 <nandhiniknandi@gmail.com>
2023-08-05 16:13:22 +0530

    code added

---
+++ src/components/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Hello = ()=>{
+    // return(
+    //     <div>
+    //         <h1>Hello Nandhini</h1>
+    //         </div>
+    // )
+    return React.createElement('div', null, React.createElement('h1', null, 'Hello Nandhini'));
+
+}
+export default Hello;
