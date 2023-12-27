71cff6f6300e4491f6f042c6519e40a98f3ea630
Nandhiniks20 <nandhiniknandi@gmail.com>
2023-12-27 12:11:54 +0530

    code added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
+import image1 from './images/image1.png';
+import image2 from './images/image2.png';
 import './App.css';
+import ReactPlayer from 'react-player';
+import { MdSmartDisplay } from "react-icons/md";
+import { TbClock } from "react-icons/tb";
 
+import React, { useRef, useState} from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Questions from './components/Question';
+import ATest from './components/Simple';
+
+
+
+
+// function App() {
+//   const playerRef = useRef(null);
+//   const VIDEO_PATH = "https://40parables-assets.s3.amazonaws.com/bleat-AI-PulseCheck-Intro.mp4";
+
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <a href="https://assessment.churchtechtoday.com/static/media/logo-left.29d686a3ec1469294cf2.png" alt='logo'>
+//         <img 
+//         src={image1}       
+//         className="App-logo"
+//          alt="logo" >
+//           </img>
+//           </a>
+//         <h1 className='h1'>AI Pluse Check</h1>
+//         <img className="logo" src={image2} alt= 'image'/>
+//       </header>
+//       <main className='main'>
+//           <div className="main-content">
+//           <div className="main-content1">
+//             <ReactPlayer ref={playerRef} url={VIDEO_PATH} controls={true} className='v1' />
+//             <div className='contect-section'>
+//             This assissment part
+//           </div>
+//           </div>        
+//           </div>
+//       </main>
+//     </div>
+//   );
+// }
+
+// export default App;
 function App() {
+  const playerRef = useRef(null);
+  const VIDEO_PATH = "https://40parables-assets.s3.amazonaws.com/bleat-AI-PulseCheck-Intro.mp4";
+  const[isPlaying, setisPlaying] = useState(false);
+
+  const handlePlayVideo = () => {
+    if (playerRef.current) {
+      if(isPlaying) {
+        playerRef.current.getInternalPlayer().pause(); // pause the video
+      }
+      else{
+        playerRef.current.getInternalPlayer().play(); // play the video
+      }
+    }
+    setisPlaying(!isPlaying);
+  };
+  
+
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a href="https://assessment.churchtechtoday.com/static/media/logo-left.29d686a3ec1469294cf2.png" alt='logo'>
+          <img 
+            src={image1}       
+            className="App-logo"
+            alt="logo"
+          />
         </a>
+        <h1 className='h1'>AI Pluse Check</h1>
+        <img className="logo" src={image2} alt='image' />
       </header>
+      <main className='main'>
+        <div className="main-content">
+          <div className="main-content1">
+            <div className="video">
+              <ReactPlayer ref={playerRef} url={VIDEO_PATH} controls={true} className='v1'  />
+            </div>
+            <div className='content-section'>
+              <div className="bottun-section">              
+              <button className="button"onClick={handlePlayVideo}>
+              <MdSmartDisplay style={{position:'absolute', width:'150%', height:'145%', top:'-7px', left:'-130px'}} />
+               Watch Video</button>
+              </div>
+              <div className="bottun-section1">
+              <h2>Is your church embracing impact <b style={{color:'black'}}>or AI* hesitant?</b></h2> 
+              <ul>
+                <li>1. Take this 3-minute assessment</li>
+                <li>2. Invite your team to take it too after you do</li>
+                <li>3. View everyone's results on 1 dashboard</li>
+                <li>4. Align on the best next step for your church's approach to AI</li>
+                <Link  to="/questions">
+                <button className='get-button' >GET STARTED</button>
+                </Link>
+                <TbClock style={{position:'relative',left:'5.4cm', top:'28px', fontSize:'80%'}}/>
+                <p style={{position:'absolute',left:'7.2cm', top:'6.9cm', fontSize:'80%', color:'black'}}>3 min</p>
+                <p className='artifical'>*Artificial Intelligence</p>
+                <p className='para'><b>If you aren't a Senior/Lead/Executive Pastor,</b> please ask one of them on your team to first take this test - ctt1.bleat.church. That test's result will generate a team link so you and other team members can then take this same test afterward as a team.</p>
+                </ul> 
+                </div>
+            </div>
+          </div>
+
+        </div>
+      </main>
+      
+
     </div>
+    <Routes>
+          <Route path="/questions" element={<Questions />} />
+        </Routes>
+    </Router>
+
   );
 }
-
 export default App;
+
---
+++ src/components/Question.js
@@ -0,0 +1,105 @@
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import React, { useState } from 'react';
+import './Questions.css';
+import { Link } from 'react-router-dom';
+
+
+const Questions = () => {
+  const questions = [
+    "",
+    "Our church's ministry strategy is firm but flexible to accommodate unexpected game changers such as AI.",
+    "Our leadership team knows of the potential risks and rewards of leveraging AI.",
+    "Our leadership team understands the value of tapping experts to educate our staff about AI.",
+  ];
+
+  const [currentQuestion, setCurrentQuestion] = useState(1);
+  const[currenQuestionState, setCurrentQuestionState] = useState(1);
+  const [showNextButton, setShowNextButton] = useState(false);
+
+  const [isprogress, setProgress] = useState(6);
+
+  const handleNext = () => {
+    if (currentQuestion < questions.length - 1) {
+      setCurrentQuestion((prev) => prev + 1);
+      setShowNextButton(false);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentQuestion > 1) {
+      setCurrentQuestion((prev) => prev - 1);
+      setShowNextButton(true);
+      setShowNextButton(false);
+
+    }
+  };
+  const handleOptionClick = () => {
+    if (currentQuestion < questions.length - 1) {
+      setCurrentQuestion((prev) => prev + 1);
+      setProgress((prevProgress) => prevProgress + 6);
+    }
+  };
+
+  return (
+    <div className="content">
+      <div className="main-content1">
+        <div className="progress-bar">
+          <div className='bar-section'>
+            <div className='bar'>
+            <div className='bar-title' style={{ position: 'absolute', left: '5cm', top: '2cm' }}>
+              STRONG
+              <progress value={isprogress} className='progress1' max="24" style={{ position: 'absolute', left: '-1.2cm', top: '-30px' }}></progress>
+              </div>
+
+            </div>
+            <div className='bar'>
+              <div className='bar-title' style={{ position: 'absolute', left: '9.7cm', top: '2cm' }}> UNDERSTANDING</div>
+              <progress value='0'  className='progress2'  style={{ position: 'absolute', left: '9.7cm', top: '45px' }}></progress>
+            </div>
+            <div className='bar'>
+              <div className='bar-title' style={{ position: 'absolute', left: '16cm', top: '2cm' }}>APPLICATION</div>
+              <progress value='0' max="24"  className='progress3'  style={{ position: 'absolute', left: '16cm', top: '45px'  }}></progress>
+            </div>
+            <div className='bar'>
+              <div className='bar-title' style={{ position: 'absolute', left:'22cm', top: '2cm' }}>DIRECTION</div>
+              <progress value='0' max="24"   className='progress2'  style={{ position: 'absolute', left: '22cm', top: '45px'  }}></progress>
+            </div>
+          
+          </div>
+        </div>
+      </div>
+      <div className="ques-slide">
+        <div className="question">
+          <p>{currentQuestion+'/'+questions.length}</p>
+          <p className={currentQuestion === 0 ? 'active' : ''}>{questions[currentQuestion]}</p>
+        </div>
+        <div className="slide-container" >
+          <div className="slide-point">
+            <div className='bulle-point' onClick={handleOptionClick} style={{ position: 'absolute', left: '1cm', top: '8cm', fontSize: '550%', cursor:'pointer' }}>.</div>
+            <div className='bulle-point' onClick={handleOptionClick} style={{ position: 'absolute', left: '5cm', top: '8cm',fontSize: '550%', cursor:'pointer' }}>.</div>
+            <div className='bulle-point' onClick={handleOptionClick} style={{ position: 'absolute', left: '9cm', top: '8cm', fontSize: '550%' }}>.</div>
+            <div className='bulle-point' onClick={handleOptionClick} style={{ position: 'absolute', left: '13cm', top: '8cm', fontSize: '550%' }}>.</div>
+            <div className='bulle-point' onClick={handleOptionClick} style={{ position: 'absolute', left: '18cm', top: '8cm', fontSize: '550%' }}>.</div>
+          </div>
+          <div className='labels'>
+            <div className='label' onClick={handleOptionClick} style={{ position: 'absolute', left: '1cm' }}>Strongly Disagree</div>
+            <div className='label' onClick={handleOptionClick} style={{ position: 'absolute', left: '5cm' }}>Disagree</div>
+            <div className='label'onClick={handleOptionClick} style={{ position: 'absolute', left: '9cm' }}>Neutral</div>
+            <div className='label' onClick={handleOptionClick} style={{ position: 'absolute', left: '13cm' }}>Agree</div>
+            <div className='label'onClick={handleOptionClick} style={{ position: 'absolute', left: '18cm' }}>Strongly Agree</div>
+          </div>
+          <Link to="/questions" className="prev" style={{positon:'absolute', left: '4cm', top:'500px'}} onClick={handlePrev}>
+          {'<---'}Prev
+        </Link>
+        {showNextButton && (
+          <button className="next" style={{position: 'absolute', left: '2cm', top:'-4cm', color:'black'}} onClick={handleNext}>
+            Next
+          </button>
+        )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Questions;
